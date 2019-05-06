8e156f8a89327e70ff1a1d913c2326c1a5c1445f
Administrator <418059810@qq.com>
2019-05-06 10:14:04 +0800

    404. 左叶子之和 递归

---
+++ LeetCode404.java
@@ -0,0 +1,23 @@
+package suanfa;
+
+public class LeetCode404 {
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root==null){
+            return 0;
+        }
+        if (root.left!=null&&root.left.left==null&&root.left.right==null){
+            return root.left.val+sumOfLeftLeaves(root.right);
+        }
+        return sumOfLeftLeaves(root.right)+sumOfLeftLeaves(root.left);
+    }
+
+}
+
+
+ class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+  }
