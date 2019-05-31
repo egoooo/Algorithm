585315947a0217ddc8d702c66617d7caa6cd86c6
Administrator <418059810@qq.com>
2019-05-31 17:30:26 +0800

    6. Z 字形变换

--- LeetCode6.java
+++ LeetCode6.java
@@ -9,6 +9,21 @@ public class LeetCode6 {
 
     }
 
+    /**
+     *将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+     *
+     * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+     *
+     * L   C   I   R
+     * E T O E S I I G
+     * E   D   H   N
+     * 之后，你的输出需要从左往右逐行读取，
+     * 产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+     * @param s
+     * @param numRows
+     * @return
+     */
+
     public static String convert(String s, int numRows) {
 
         if (numRows==1)return s;
