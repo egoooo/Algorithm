f711e3af646201010d185f1c297e4b8abdabb363
Administrator <418059810@qq.com>
2019-05-31 17:29:19 +0800

    56. 合并区间 中等难度，感觉很经典也

--- LeetCode56.java
+++ LeetCode56.java
@@ -14,6 +14,17 @@ public class LeetCode56 {
 
     }
 
+    /**
+     * 给出一个区间的集合，请合并所有重叠的区间。
+     *
+     * 示例 1:
+     *
+     * 输入: [[1,3],[2,6],[8,10],[15,18]]
+     * 输出: [[1,6],[8,10],[15,18]]
+     * 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+     * @param intervals
+     * @return
+     */
     public static int[][] merge(int[][] intervals) {
         if (intervals.length <= 1)
             return intervals;
